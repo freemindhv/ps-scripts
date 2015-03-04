1dd9089114700f360440b3339a36f5dbede3384d
d.greiner@highvillage.de <gOthik1804>
2015-03-04 18:10:38 +0100

    removed variable removed variable $folderpath, unnecessary added Comment for $faileditems

--- pstImport.ps1
+++ pstImport.ps1
@@ -10,12 +10,11 @@
 
 $mailbox = "j.doe"                        # Mailbox in which the pst files will be imported
 $basefolder = "\\localhost\C`$\TEMP\"     # UNC Path to the folder where the mailbox folder lies
-$archive = $true                           # Import into personal archive? False will import into the users Mailbox
-$faileditems = 5
+$archive = $true                          # Import into personal archive? False will import into the users Mailbox
+$faileditems = 5                          # Set how many Errors can occur before the import will be aborted
 
 
-$folderpath = $basefolder + $mailbox
-$files = Get-ChildItem -Path $folderpath -Filter *.pst
+$files = Get-ChildItem -Path ($basefolder + $mailbox) -Filter *.pst
 $counter = 0
 
 foreach ($pst in $files) {
